b7fa05d96977da97fd5b6247da64fc3f6c7e0ba2
idhemR <mettour.mehdi@gmail.com>
2023-06-18 14:01:30 -0700

    uppercase lowercase alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int AZ;
 
-for (AZ = 'A'; AZ <= 'Z'; AZ++)
-putchar(AZ);
 for (AZ = 'a'; AZ <= 'z'; AZ++)
+putchar(AZ);
+for (AZ = 'A'; AZ <= 'Z'; AZ++)
 putchar(AZ);
 putchar('\n');
 return (0);
