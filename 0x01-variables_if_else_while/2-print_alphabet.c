d18adb818dece02e2dc27a3a20912443621b2f7e
idhemR <mettour.mehdi@gmail.com>
2023-06-18 13:27:03 -0700

    digit % 10 of random n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - display the alphabet in lowercase,
+*
+* Return: 0 is valid
+*/
+int main(void)
+{
+char AZ;
+
+for (AZ = 'a'; AZ <= 'z'; AZ++)
+{
+putchar(AZ);
+}
+putchar('\n');
+return (0);
+}
+
