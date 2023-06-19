0232a641c0d9865a9461b7a85a052fafb2f3063a
idhemR <mettour.mehdi@gmail.com>
2023-06-19 06:56:55 -0700

    digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - display single digit of base 10
+*
+* Return: 0 is valid
+*/
+int main(void)
+{
+int AZ = 0;
+
+while (AZ < 10)
+{
+printf("%d", AZ);
+AZ++;
+}
+putchar('\n');
+return (0);
+}
