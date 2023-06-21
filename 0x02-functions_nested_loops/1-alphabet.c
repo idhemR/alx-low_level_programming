6fc7d2200e11488ab0cc93cf49e02b7f9690ea1a
idhemR <mettour.mehdi@gmail.com>
2023-06-21 15:39:54 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+char AZ;
+
+for (AZ = 'a'; AZ <= 'z'; AZ++)
+_putchar(AZ);
+
+_putchar('\n');
+}
