bd82bb75961e18e93289925802465806fe42c954
idhemR <mettour.mehdi@gmail.com>
2023-06-22 14:36:49 -0700

    isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _is upper - check if the letter is uppercase.
+*
+* Return:  1 if uppercase 0 if lowercase.
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90 )
+return (1);
+else 
+return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c);
+int _putchar(char c);
+
+#endif
