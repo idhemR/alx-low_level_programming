7b4fddc21263857b115616120451cb9250f530f0
idhemR <mettour.mehdi@gmail.com>
2023-06-23 07:47:11 -0700

    print _ line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_line - print underscore
+*
+* @b : nbr of _
+*
+* return : void
+*
+*/
+
+void print_line(int b)
+{
+int a = 20;
+int b = 0;
+while (b < a)
+{
+_putchar('_');
+b++;
+
+}
+
+
+
+}
