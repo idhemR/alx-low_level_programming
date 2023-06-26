2eb6abb16a3b6fd27c79871d14247002f323cb56
idhemR <mettour.mehdi@gmail.com>
2023-06-26 15:58:48 -0700

    swaping pointer value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+* swap_int - swap number values
+*
+* @a : first value
+* @b : second value
+*
+* return : void
+*/
+
+
+void swap_int(int *a, int *b)
+{
+
+int c;
+c = *a;
+*a = *b;
+*b = c;
+
+
+
+}
