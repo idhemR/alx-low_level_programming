65bd8437ae3a10985b85ac44f852384b29dba5ab
idhemR <mettour.mehdi@gmail.com>
2023-06-27 16:42:55 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_rev - print the string in reverse
+*
+* @s : the string parameter
+*
+* return : nothing
+*/
+
+
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++
+while (i--)
+{
+_putchar(s[i]);
+}
+
+_putchar('\n');
+
+}
