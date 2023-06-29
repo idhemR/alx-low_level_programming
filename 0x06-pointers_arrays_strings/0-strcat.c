645ceeaa31d2388edadb7ca56db48f5e0f11cf75
idhemR <mettour.mehdi@gmail.com>
2023-06-29 14:29:39 -0700

    concat 2 str

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
 * _strcat - concatenates two strings
-* @dest: input value
-* @src: input value
+* @dest: destination
+* @src: source
 *
-* Return: void
+* Return: dest
 */
 char *_strcat(char *dest, char *src)
 {
