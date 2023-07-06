7afe9794b4a976622b064ab7d6ae2334769f8f42
idhemR <mettour.mehdi@gmail.com>
2023-07-06 06:30:51 -0700

    prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string
+ *
+ *@s: input value
+ *
+ *return: void
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+if (s == '\0')
+{
+_putchar('\n');
+while (s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+
+}
+}
+
+
+}
